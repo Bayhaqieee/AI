96b40d54b47c741551ecc6a3a9737e700290307f
Muhammad Aditya Bayhaqie <adityabayhaqie@gmail.com>
2024-08-21 13:56:48 +0800

    Learning - PTS - DataFrame Preparation File COnversion

---
+++ Python Training Series/Pandas/Pandas_DataFrame_Preparation.py
@@ -0,0 +1,11 @@
+
+# ### DataFrame Preparation
+# #### Import pandas under the alias pd
+
+import pandas as pd
+# #### Print the version of pandas that has been imported
+
+print(pd.__version__)
+# #### Print out all the version information of the libraries that are required by the pandas library:
+
+pd.show_versions()
