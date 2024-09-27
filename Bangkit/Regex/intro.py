66e8ea4e47e5e9b6f1bb31be3ef2d91248aea3e5
Muhammad Aditya Bayhaqie <adityabayhaqie@gmail.com>
2024-09-27 11:17:50 +0800

    Bangkit - Crash Course

--- Bangkit/Regex/intro.py
+++ Bangkit/Regex/intro.py
@@ -11,3 +11,14 @@ log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package 
 regex = r"\[(\d+)\]"
 result = re.search(regex, log)
 print(result[1])
+
+result1 = re.search(r"aza", "plaza")
+print(result1)
+
+result2 = re.search(r"aza", "bazaar")
+print(result2)
+
+result3 = re.search(r"aza", "maze")
+print(result3)
+
+print(re.search(r"^x", "xenon"))
