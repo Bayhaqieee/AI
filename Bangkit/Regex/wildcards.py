85764c5c038a40ba75d7e3994b0f2c651e7f1ed3
Muhammad Aditya Bayhaqie <adityabayhaqie@gmail.com>
2024-09-27 11:29:52 +0800

    Bangkit - Crash Course

--- Bangkit/Regex/wildcards.py
+++ Bangkit/Regex/wildcards.py
@@ -17,3 +17,15 @@ print(re.search(r"cat|dog", "I like cats."))
 print(re.search(r"cat|dog", "I love dogs!"))
 print(re.search(r"cat|dog", "I like both dogs and cats."))
 print(re.findall(r"cat|dog", "I like both dogs and cats."))
+
+print(re.search(r"Py.*n", "Pygmalion"))
+print(re.search(r"Py.*n", "Python Programming"))
+print(re.search(r"Py[a-z]*n", "Python Programming"))
+print(re.search(r"Py[a-z]*n", "Pyn"))
+
+print(re.search(r"o+l+", "goldfish"))
+print(re.search(r"o+l+", "woolly"))
+print(re.search(r"o+l+", "boil"))
+
+print(re.search(r"p?each", "To each their own"))
+print(re.search(r"p?each", "I like peaches"))
