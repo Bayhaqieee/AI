f8031504719a5f1c337bc1cb1ede528984df2ed5
Muhammad Aditya Bayhaqie <adityabayhaqie@gmail.com>
2024-09-27 23:38:11 +0800

    Bangkit - Regex and OS

--- Bangkit/Regex/advanceregex.py
+++ Bangkit/Regex/advanceregex.py
@@ -27,3 +27,29 @@ def rearrange_name2(name):
         return name
     return "{} {}".format(result[2], result[1])
 print(rearrange_name2("Hopper, Grace M."))
+
+print(re.search(r"[a-zA-Z]{5}", "a ghost"))
+print(re.search(r"[a-zA-Z]{5}", "a scary ghost appeared"))
+print(re.findall(r"[a-zA-Z]{5}", "a scary ghost appeared"))
+print(re.findall(r"\w{5,10}", "I really like strawberries"))
+print(re.findall(r"\w{5,}", "I really like strawberries"))
+print(re.search(r"s\w{,20}", "I really like strawberries"))
+
+log = "July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade"
+regex = r"\[(\d+)\]"
+result = re.search(regex, log)
+result = re.search(regex, "A completely different string that also has numbers [34567]")
+result = re.search(regex, "99 elephants in a [cage]")
+def extract_pid(log_line):
+    regex = r"\[(\d+)\]"
+    result = re.search(regex, log_line)
+    if result is None:
+        return ""
+    return result[1]
+print(extract_pid(log))
+print(extract_pid("99 elephants in a [cage]"))
+
+print(re.split(r"[.?!]", "One sentence. Another one? And the last one!"))
+print(re.split(r"([.?!])", "One sentence. Another one? And the last one!"))
+print(re.sub(r"[\w.%+-]+@[\w.-]+", "[REDACTED]", "Received an email for go_nuts95@my.example.com"))
+print(re.sub(r"^([\w .-]*), ([\w .-]*)$", r"\2 \1", "Lovelace, Ada"))
